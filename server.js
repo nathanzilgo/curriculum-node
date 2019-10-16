bf78ee97ba921497e35becb086df7631b8e0aced
nathan <nathanfpedroza@gmail.com>
2019-10-15 23:02:44 -0300

    fixing bugs

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (request, response) => {
-    var curriculum = "../lib/historico_118110342_203258685.pdf";
+    var curriculum = "./lib/historico_118110342_203258685.pdf";
 
     fs.readFile(curriculum, (err, data) => {
         response.contentType("application/pdf");
